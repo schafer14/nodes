970875ff921c2192586fcc8b0477b11018847d48
Banner <banner.schafer@gmail.com>
2014-01-27 20:16:38 -0800

    conflict res

--- my_modules/tests/test-currency.js
+++
@@ -1,4 +0,0 @@
-var currency = require('./currency');
-
-console.log('50 Canadian dollars is equivalent to: ' + currency.canadianToUS(50) + ' USD');
-console.log('50 USD is equivalent to: ' + currency.USToCanadian(50) + ' Canadian dollars');
